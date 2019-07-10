f2b382b074f30c1c781b66a389b76f6cd2021f5c
Shaowei <Shaowei.Zhang@Outlook.com>
2019-07-10 21:27:42 +1000

    Move player horizontal

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float runSpeed = 5f;
+    private Rigidbody2D _myRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+    }
+
+    private void Run()
+    {
+        var controlThrow = CrossPlatformInputManager.GetAxis("Horizontal") * runSpeed; // -1 ~ +1
+        var playerVelocity = new Vector2(controlThrow, _myRigidbody.velocity.y);
+        _myRigidbody.velocity = playerVelocity;
+    }
+}
