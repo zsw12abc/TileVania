b82e42ef823f9a54b47319c5b6892cbe8d9b5e11
Shaowei <Shaowei.Zhang@Outlook.com>
2019-07-16 21:56:24 +1000

    Making Enemy

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+
+    private Rigidbody2D _MyRigidbody2D;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _MyRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsFacingRight())
+        {
+            _MyRigidbody2D.velocity = new Vector2(moveSpeed, 0f);
+        }
+        else
+        {
+            _MyRigidbody2D.velocity = new Vector2(-moveSpeed, 0f);
+        }
+    }
+
+    private bool IsFacingRight()
+    {
+        return transform.localScale.x > 0;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(_MyRigidbody2D.velocity.x)), 1f);
+    }
+}
